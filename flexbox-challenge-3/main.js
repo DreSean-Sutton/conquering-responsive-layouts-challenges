f09ee6e4992b8f799da9113c5463ee548c58fb15
DreSean-Sutton <dreseansutton@gmail.com>
2022-01-16 14:53:03 -0800

    Set up flexbox-challenge-3 and corrected the text-content in the title element tags on all index.html files

---
+++ flexbox-challenge-3/main.js
@@ -0,0 +1,15 @@
+let $link = document.querySelector('#link');
+let $contentLayout = document.querySelector('#content-layout');
+let $modal = document.querySelector('#modal');
+
+$link.addEventListener('click', function (event) {
+  event.preventDefault();
+  $modal.classList.remove('hidden');
+})
+
+$modal.addEventListener('click', function (event) {
+  if (event.target.matches('#no-button')) {
+    event.preventDefault();
+    $modal.classList.add('hidden');
+  }
+})
