d876af5b52f2b265f4b50cec65d9f263a4418397
DreSean-Sutton <dreseansutton@gmail.com>
2022-01-27 18:01:41 -0800

    FInished navbar at bottom currently on navbar at top and setting up overlay

--- final-challenge/main.js
+++ final-challenge/main.js
@@ -1,12 +1,28 @@
-var $navItem = document.querySelector('#header-nav');
-var $CRLLogo = document.querySelector('#logo');
+let $navItem = document.querySelector('#header-nav');
+let $CRLLogo = document.querySelector('#logo');
+let $bottomNavbar = document.querySelector('.navbar-icon--bottom');
+let $navSectionMobile = document.querySelector('#navbar-section__mobile');
+let $overlay = document.querySelector('.overlay');
 
-$CRLLogo.addEventListener('click', toggleNavbar)
+$CRLLogo.addEventListener('click', toggleTopNavbar);
+$bottomNavbar.addEventListener('click', toggleBottomNavbar);
 
-function toggleNavbar (event) {
+
+function toggleTopNavbar (event) {
   if ((!$navItem.className.includes('hidden')) && (!$navItem.className.includes('desktop'))) {
     $navItem.classList.add('hidden', 'nav__mobile');
+    $overlay.classList.add('hidden');
   } else {
     $navItem.classList.remove('hidden', 'nav__mobile');
+    $overlay.classList.remove('hidden');
+  }
+}
+
+function toggleBottomNavbar (event) {
+  if ($navSectionMobile.className.includes('hidden')) {
+    $navSectionMobile.classList.remove('hidden');
+  } else {
+    $navSectionMobile.classList.add('hidden');
+    $overlay.classList.add('hidden');
   }
 }
